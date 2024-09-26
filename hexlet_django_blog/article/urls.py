544ba05d0781968e8d8e6cb8c9fe577035cbd171
Ihar <kostyuchik22@gmail.com>
2024-09-26 10:30:21 +0300

    lesson 14

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from hexlet_django_blog.article import views
-from hexlet_django_blog.article.views import IndexView, ArticleByID
+from hexlet_django_blog.article.views import IndexView, ArticleByID, ArticleView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='article'),
-    path('<str:tags>/<int:article_id>/', ArticleByID.as_view(), name='article_id'),
+    path('<int:id>/', ArticleView.as_view(), name='article_id'),
+    path('<str:tags>/<int:article_id>/', ArticleByID.as_view()),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from hexlet_django_blog.article.models import Article
 
 
@@ -8,6 +8,12 @@ class IndexView(View):
     def get(self, request, *args, **kwargs):
         articles = Article.objects.all()[:15]
         return render(request, 'articles/index.html', context={'articles': articles})
+
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={'article': article})
 
 
 class ArticleByID(View):

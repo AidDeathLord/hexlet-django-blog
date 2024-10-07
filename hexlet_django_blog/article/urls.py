6fa3d25f9afd75c0ecf3acdcfcdac8b834fe3413
Ihar <kostyuchik22@gmail.com>
2024-10-07 09:35:33 +0300

    end

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 # from hexlet_django_blog.article import views
-from hexlet_django_blog.article.views import IndexView, ArticleByID, ArticleView
+from hexlet_django_blog.article.views import IndexView, ArticleByID, ArticleView, ArticleFormCreateView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='article'),
+    path('', IndexView.as_view(), name='articles'),
     path('<int:id>/', ArticleView.as_view(), name='article_id'),
     path('<str:tags>/<int:article_id>/', ArticleByID.as_view()),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
 from django.views import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
 
 
 class IndexView(View):
@@ -21,6 +22,18 @@ class ArticleByID(View):
         return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
 
 
+class ArticleFormCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():  # Если данные корректные, то сохраняем данные формы
+            form.save()
+            return redirect('articles')  # Редирект на указанный маршрут
+        # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
+        return render(request, 'articles/create.html', {'form': form})
 # def index(request):
 #     return HttpResponse('article')
 
